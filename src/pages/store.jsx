3819f23632266d3b58791a474ee43c1e6835c836
ReneSzz <renesuar99@gmail.com>
2023-11-22 22:06:38 -0500

    Setup fetch call to get items

--- src/pages/store.jsx
+++ src/pages/store.jsx
@@ -4,9 +4,11 @@ import { useEffect, useState } from 'react'
 export function Store()
 {
     let [check, setCheck] = useState(false);
+    //change to an array of objects
     let [item, setItem] = useState({});
          
   useEffect(()=> { async function getItem(){
+    //create loop to retrieve multiple items
         fetch('https://fakestoreapi.com/products/1')
         .then(res=>res.json())
         .then(json=>setItem(json))
