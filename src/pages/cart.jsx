bfdc17ce89f6bd1d81706f0ec4790250fd6aa111
ReneSzz <renesuar99@gmail.com>
2023-11-24 15:07:44 -0500

    made items in cart smaller

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -18,18 +18,18 @@ export function Cart({itemsinCart}) {
           { itemsinCart.map((items) =>{
           
             return (
-             <Col xs> 
-             <Card bg="dark" text="light" className="cardFix" style={{ width: '12rem' }}>
-             <Card.Img variant="top" src={items.image} width={12} height={180} />
+            
+             <Card bg="dark" text="light" className="cardFix" style={{ width: '9rem', height:'8rem', flexDirection:'row'}}>
+             <Card.Img variant="top" src={items.image} style={{ width: '3rem', height:'50px'}} />
              <Card.Body>
-               <Card.Title variant="light" className="cardTitleFix">{items.title}</Card.Title>
+               <Card.Title variant="light" className="cardTitleFix" style={{ fontSize:'0.8rem'}}>{items.title}</Card.Title>
                <Card.Text>
                  ${items.price}
                </Card.Text>
 
              </Card.Body>
            </Card>
-           </Col>
+
          
              )
            
